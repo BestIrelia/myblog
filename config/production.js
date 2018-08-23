0331384edb854ead552b8601038630028427ae62
mosisen <mosisen@512yc.cn>
2018-08-23 17:14:52 +0800

    rm production

--- config/production.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: 80,
-    session: {
-        secret: 'myblog',
-        key: 'myblog',
-        maxAge: 2592000000
-    },
-    mongodb: 'mongodb://172.17.0.3:27017/myblog'
-}
